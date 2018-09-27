2d5558aae186c199e574d8c31d47a32688794aea
smurg <smurgolo@growthaccelerationpartners.com>
2018-09-27 19:01:17 -0300

    add code to do backend search but with debounce functionality using rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule, MatInputModule } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatToolbarModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
